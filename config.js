2899a10a5b3ba88158e6cb9426e530a968f2b3d7
Sunil Prakash <sunil.thind17@gmail.com>
2019-12-07 13:05:48 +0530

    My chat application

---
+++ config.js
@@ -0,0 +1,7 @@
+/** swap out */
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/b24ce18e-41dd-4e23-9fd3-8b1c6c064f24/token";
+
+/** swap out */
+const instanceLocator = "v1:us1:b24ce18e-41dd-4e23-9fd3-8b1c6c064f24";
+
+export { tokenUrl, instanceLocator }
